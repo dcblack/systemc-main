425bb85ad63d5b6002f8454586824c9282c59aa2
David C Black <david.black@doulos.com>
2018-08-27 15:46:55 -0500

    Use namespaces to simplify code. Add C++11 use of unique_ptr and deletion for older versions

--- basic_main.cpp
+++ basic_main.cpp
@@ -1,29 +1,42 @@
 //FILE basic_main.cpp (systemc)
 // $Info: Entry point for executing simulation for 'basic'.$
 //-----------------------------------------------------------------------------
-//#include "main.h"
 #include "basic_top.h"
 #include "wallclock.h"
+#if __cplusplus >= 201103L
+#  include <memory>
+#endif
 #include "report.h"
 std::ostringstream mout;
 
 namespace {
-  const char* MSGID = "/Doulos Inc/{:PROJECT:}/main";
-  double elaboration_time=-1.0, starting_time=-1.0, finished_time=-1.0;
+  const char* MSGID = "/Doulos Inc/SystemC-Example/main";
+  double elaboration_cpuTime=-1.0, starting_cpuTime=-1.0, finished_cpuTime=-1.0;
   int summary(void);
 }
 
+using namespace sc_core;
+using namespace std;
+
 int sc_main(int argc, char *argv[])
 {
+#if __cplusplus < 201103L
   Basic_top* basic_top;
+#else
+  unique_ptr<Basic_top> basic_top;
+#endif
   // Elaborate
-  elaboration_time = get_cpu_time();
+  elaboration_cpuTime = get_cpu_time();
   try {
+#if __cplusplus < 201103L
     basic_top = new Basic_top("basic_top");
-  } catch (sc_core::sc_exception& e) {
+#else
+    basic_top.reset(new Basic_top("basic_top"));
+#endif
+  } catch (sc_exception& e) {
     REPORT(INFO,"\n" << e.what() << "\n\n*** Please fix elaboration errors and retry. ***");
     return summary();
-  } catch (std::exception& e) {
+  } catch (exception& e) {
     REPORT(INFO,"\n" << e.what() << "\n\n*** Please fix elaboration errors and retry. ***");
     return summary();
   } catch (...) {
@@ -32,26 +45,28 @@ int sc_main(int argc, char *argv[])
   }//endtry
 
   // Simulate
+  sc_time finished_simTime;
   try {
     REPORT(INFO,"Starting kernel");
-    starting_time = get_cpu_time();
-    sc_core::sc_start();
-    finished_time = get_cpu_time();
-    REPORT(INFO,"Exited kernel at " << sc_core::sc_time_stamp());
-  } catch (sc_core::sc_exception& e) {
+    starting_cpuTime = get_cpu_time();
+    sc_start();
+    finished_simTime = sc_time_stamp();
+    finished_cpuTime = get_cpu_time();
+  } catch (sc_exception& e) {
     REPORT(WARNING,"\n\nCaught exception during active simulation.\n" << e.what());
-  } catch (std::exception& e) {
+  } catch (exception& e) {
     REPORT(WARNING,"\n\nCaught exception during active simulation.\n" << e.what());
   } catch (...) {
     REPORT(WARNING,"Error: Caught unknown exception during active simulation.");
   }//endtry
+  REPORT(INFO,"Exited kernel at " << finished_simTime);
 
   // Clean up
-  if (! sc_core::sc_end_of_simulation_invoked()) {
+  if (! sc_end_of_simulation_invoked()) {
     try {
       REPORT(INFO,"ERROR: Simulation stopped without explicit sc_stop()");
-    } catch(sc_core::sc_exception& e) { REPORT(INFO,"\n\n" << e.what()); }
-    sc_core::sc_stop(); //< this will invoke end_of_simulation() callbacks
+    } catch(sc_exception& e) { REPORT(INFO,"\n\n" << e.what()); }
+    sc_stop(); //< this will invoke end_of_simulation() callbacks
   }//endif
 
   return summary();
@@ -63,22 +78,22 @@ namespace {
   // Summarize results
   int summary(void)
   {
-    std::string kind = "Simulation";
-    if ( starting_time < 0.0 ) {
+    string kind = "Simulation";
+    if ( starting_cpuTime < 0.0 ) {
       kind = "Elaboration";
-      starting_time = finished_time = get_cpu_time();
+      starting_cpuTime = finished_cpuTime = get_cpu_time();
     } 
-    if ( finished_time < 0.0 ) {
-      finished_time = get_cpu_time();
+    if ( finished_cpuTime < 0.0 ) {
+      finished_cpuTime = get_cpu_time();
     }
-    auto errors = sc_core::sc_report_handler::get_count(sc_core::SC_ERROR)
-                + sc_core::sc_report_handler::get_count(sc_core::SC_FATAL);
-    REPORT(INFO, "\n" << std::string(80,'#') << "\nSummary for " << sc_core::sc_argv()[0] << ":\n  "
-      << "CPU elaboration time " << std::setprecision(4) << (starting_time - elaboration_time) << " sec\n  "
-      << "CPU simulation  time " << std::setprecision(4) << (finished_time - starting_time) << " sec\n  "
-      << std::setw(2) << sc_core::sc_report_handler::get_count(sc_core::SC_WARNING) << " warnings" << "\n  "
-      << std::setw(2) << sc_core::sc_report_handler::get_count(sc_core::SC_ERROR)   << " errors"   << "\n  "
-      << std::setw(2) << sc_core::sc_report_handler::get_count(sc_core::SC_FATAL)   << " fatals"   << "\n\n"
+    auto errors = sc_report_handler::get_count(SC_ERROR)
+                + sc_report_handler::get_count(SC_FATAL);
+    REPORT(INFO, "\n" << string(80,'#') << "\nSummary for " << sc_argv()[0] << ":\n  "
+      << "CPU elaboration time " << setprecision(4) << (starting_cpuTime - elaboration_cpuTime) << " sec\n  "
+      << "CPU simulation  time " << setprecision(4) << (finished_cpuTime - starting_cpuTime) << " sec\n  "
+      << setw(2) << sc_report_handler::get_count(SC_WARNING) << " warnings" << "\n  "
+      << setw(2) << sc_report_handler::get_count(SC_ERROR)   << " errors"   << "\n  "
+      << setw(2) << sc_report_handler::get_count(SC_FATAL)   << " fatals"   << "\n\n"
       << kind << " " << (errors?"FAILED":"PASSED")
     );
     return (errors?1:0);
