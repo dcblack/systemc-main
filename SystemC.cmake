819adbc200648a4e5228d5d3199b1ffd7d5b364d
David C Black <david.black@doulos.com>
2020-06-05 08:57:43 -0500

    local systemc file in case default location not used

--- SystemC.cmake
+++ SystemC.cmake
@@ -11,6 +11,10 @@ link_libraries(SystemC::systemc)
 
 # Things that affect all targets
 set(shared_env LD_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib DYLD_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib)
+
+#find_package( Boost 1.65 REQUIRED )
+#find_package( yaml-cpp CONFIG REQUIRED )
+
 add_custom_target(run
    time env ${shared_env} ./${TARGET_EXECUTABLE} $(ARGS) 2>&1 | tee {:NAME:}.log
    COMMENT "Running program ${TARGET_EXECUTABLE}"
